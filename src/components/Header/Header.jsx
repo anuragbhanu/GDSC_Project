a578b489b56327c894045d3df93e94761918d9eb
Raunak <tiwariraunakraj05@gmail.com>
2024-02-11 14:13:27 +0530

    updated files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,20 +8,20 @@ export default function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12"
+                            src="./images/L11.png"
+                            className="mr-3 h-18 w-24 p"
                             alt="Logo"
                         />
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
+                            to="SignIn"
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
                         </Link>
                         <Link
-                            to="#"
+                            to="/SignUp"
                             className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Join Us
@@ -60,6 +60,17 @@ export default function Header() {
                                     }
                                 >
                                     
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink
+                                to="/Authorities"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Authorities
                                 </NavLink>
                             </li>
                             
